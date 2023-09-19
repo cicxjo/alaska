42c003d7e8cfdb231741e02828f438956f2d1ec0
Cyril Augier <cicxjo@posteo.net>
2023-09-19 15:49:46 +0200

    Ajoute le support des URLs complexes dans le routeur

--- App/Model/Router.php
+++ App/Model/Router.php
@@ -22,18 +22,47 @@ class Router
         return $this;
     }
 
+    private function transformUriToArray(string $uri): array
+    {
+        return array_values(array_filter(explode('/', $uri)));
+    }
+
+    private function extractParameters(array $route): array
+    {
+        $uri = $this->transformUriToArray($this->uri);
+        $route = $this->transformUriToArray($route['uri']);
+
+        array_shift($uri);
+        array_shift($route);
+
+        foreach ($route as $key => $value) {
+            $value = str_replace(':', '', $value);
+            $parameters[$value] = $uri[$key];
+        }
+
+        return $parameters; // associative array
+    }
+
     private function matchSimpleRoute(array $route): bool
     {
         return $route['uri'] === $this->uri;
     }
 
-    private function callController(array $controller): void
+    private function matchComplexRoute(array $route): bool
+    {
+        $uri = $this->transformUriToArray($this->uri);
+        $route = $this->transformUriToArray($route['uri']);
+
+        return count($uri) === count($route) && $uri[0] === $route[0];
+    }
+
+    private function callController(array $controller, array $args = []): void
     {
         $class = $controller[0];
         $method = $controller[1];
 
         $class = new $class();
-        $class->$method();
+        $class->$method($args);
     }
 
     public function run(): void
@@ -41,6 +70,10 @@ class Router
         foreach ($this->routes as $route) {
             if ($this->matchSimpleRoute($route)) {
                 $this->callController($route['controller']);
+                return;
+            } elseif ($this->matchComplexRoute($route)) {
+                $parameters = $this->extractParameters($route);
+                $this->callController($route['controller'], $parameters);
                 return;
             }
         }
